2a261c71589d52246197211e75b7570d2a8d8919
John Nunley <jtnunley01@gmail.com>
2023-06-11 08:27:36 -0700

    Reimplement using 100% safe code (#7)
    Uses the Wake trait to eliminate all unsafe code in this crate.

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,14 @@
 //! A [`Waker`] is just a fancy callback. This crate converts regular closures into wakers.
 
 #![no_std]
+#![forbid(unsafe_code)]
 #![warn(missing_docs, missing_debug_implementations, rust_2018_idioms)]
 
 extern crate alloc;
 
 use alloc::sync::Arc;
-use core::mem::{self, ManuallyDrop};
-use core::task::{RawWaker, RawWakerVTable, Waker};
+use alloc::task::Wake;
+use core::task::Waker;
 
 /// Converts a closure into a [`Waker`].
 ///
@@ -26,38 +27,17 @@ use core::task::{RawWaker, RawWakerVTable, Waker};
 /// waker.wake();        // Prints "woken".
 /// ```
 pub fn waker_fn<F: Fn() + Send + Sync + 'static>(f: F) -> Waker {
-    let raw = Arc::into_raw(Arc::new(f)) as *const ();
-    let vtable = &Helper::<F>::VTABLE;
-    unsafe { Waker::from_raw(RawWaker::new(raw, vtable)) }
+    Waker::from(Arc::new(Helper(f)))
 }
 
 struct Helper<F>(F);
 
-impl<F: Fn() + Send + Sync + 'static> Helper<F> {
-    const VTABLE: RawWakerVTable = RawWakerVTable::new(
-        Self::clone_waker,
-        Self::wake,
-        Self::wake_by_ref,
-        Self::drop_waker,
-    );
-
-    unsafe fn clone_waker(ptr: *const ()) -> RawWaker {
-        let arc = ManuallyDrop::new(Arc::from_raw(ptr as *const F));
-        let _arc_clone: mem::ManuallyDrop<_> = arc.clone();
-        RawWaker::new(ptr, &Self::VTABLE)
+impl<F: Fn() + Send + Sync + 'static> Wake for Helper<F> {
+    fn wake(self: Arc<Self>) {
+        (self.0)();
     }
 
-    unsafe fn wake(ptr: *const ()) {
-        let arc = Arc::from_raw(ptr as *const F);
-        (arc)();
-    }
-
-    unsafe fn wake_by_ref(ptr: *const ()) {
-        let arc = ManuallyDrop::new(Arc::from_raw(ptr as *const F));
-        (arc)();
-    }
-
-    unsafe fn drop_waker(ptr: *const ()) {
-        drop(Arc::from_raw(ptr as *const F));
+    fn wake_by_ref(self: &Arc<Self>) {
+        (self.0)();
     }
 }
